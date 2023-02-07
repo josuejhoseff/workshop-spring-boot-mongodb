e83c46cf0ef4b7011b9ab8ac4a4fc4fb8781b9b5
Josué Jhoseff <josuejhosefff@gmail.com>
2023-02-07 15:16:15 -0300

    Obtendo usuário por id e trantamento de exceptions

--- src/main/java/com/worshopmongo/repository/UserRepository.java
+++ src/main/java/com/worshopmongo/repository/UserRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.worshopmongo.domain.User;
-
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
 	
