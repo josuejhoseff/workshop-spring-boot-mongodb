ce8b0a294a90380a95d7c74b1a639248f8517c66
Josué Jhoseff <josuejhosefff@gmail.com>
2023-02-08 16:07:43 -0300

    Obtendo um post por id

---
+++ src/main/java/com/worshopmongo/resources/PostResource.java
@@ -0,0 +1,36 @@
+package com.worshopmongo.resources;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.worshopmongo.domain.Post;
+import com.worshopmongo.domain.User;
+import com.worshopmongo.dto.UserDTO;
+import com.worshopmongo.services.PostService;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+	
+	
+	@Autowired
+	private PostService service;
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/{id}")
+	//@GetMapping
+	public ResponseEntity<Post> findById(@PathVariable String id){
+		Post obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+}
